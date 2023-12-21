2c13d317e5d1d6aeaa3e74b6f8b91d1f6cdcb470
Robert Angote <ebobby@outlook.com>
2023-12-21 14:59:32 +0300

    Set Matrix Zeros C# algorithm.

--- Console75/Program.cs
+++ Console75/Program.cs
@@ -7,15 +7,15 @@ using Console75;
 
 //Test Case
 //ListNode edges = '1-> 2 -> 3 -> 4 -> 5';
-ListNoder page = [[1, 4, 5], [1, 3, 4], [2, 6]];
-int x = 5;
+//ListNoder page = [[1, 4, 5], [1, 3, 4], [2, 6]];
+//int x = 5;
 
 //string[] wordDictNew = {"leet","code"};
 
-MergedK_List mergedK_List = new MergedK_List(); 
-var xx = mergedK_List.MergedK_ListSol( page);
+//MergedK_List mergedK_List = new MergedK_List(); 
+//var xx = mergedK_List.MergedK_ListSol( page);
 
-Console.WriteLine(xx.ToString());
+//Console.WriteLine(xx.ToString());
 
 
 //var res = combiSum.CombiSumSol(numz, target);

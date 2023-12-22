0f6d7114ed51d8601c231c535f2eb5e60964aa06
Robert Angote <ebobby@outlook.com>
2023-12-22 11:06:42 +0300

    Longest Substring Without Repeating Characters C# algorithm implementation.

---
+++ Console75/LongestSubString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console75
+{
+    public class LongestSubString
+    {
+        /*
+         * Time Complexity O(n), where n is the length of the input string. The algorithm performs a single pass through the string.
+         * 
+         * Space Complexity O(min(m, n)), where m is the size of the character set (constant) and n is the length of the input string.
+         * The space used by the lastIndexMap is bounded by the size of the character set.
+         * 
+         * Given a string s, find the length of the longest substring without repeating characters.
+         * 
+         * The code uses a sliding window approach with two pointers (left and right) to track the longest substring without repeating characters.
+           It maintains a lastIndexMap to store the last index of each character in the current substring.
+           If a character repeats and its last index is greater than or equal to the left pointer, the left pointer is updated to the next position after the last occurrence of the character.
+           The maximum length is updated during each iteration.
+         * 
+         */
+
+        public int LongestSubStringSol(string s) 
+        {
+            int n = s.Length;
+            int maxLength = 0;
+            int left = 0;
+
+            Dictionary<char, int> lastIndexMap = new Dictionary<char, int>();
+
+            for (int right = 0; right < n; right++) 
+            {
+                if (lastIndexMap.ContainsKey(s[right]) && lastIndexMap[s[right]] >= left) 
+                {
+                    //Update the left pointer if the character is repeated
+                    left = lastIndexMap[s[right]] + 1;
+                }
+
+                //Update the maximum length
+                maxLength = Math.Max(maxLength, right - left + 1);
+
+                //Update the last index of the current character
+                lastIndexMap[s[right]] = right;
+            }
+            return maxLength;
+
+        }
+    }
+}
--- Console75/Program.cs
+++ Console75/Program.cs
@@ -8,15 +8,18 @@ using Console75;
 //Test Case
 //ListNode edges = '1-> 2 -> 3 -> 4 -> 5';
 int[][] page = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-//int x = 5;
+string x = "bbbbb";
+
+LongestSubString longestSubString = new LongestSubString();
+//longestSubString.LongestSubStringSol(x);
 
 //string[] wordDictNew = {"leet","code"};
 
 RotateImage sm = new RotateImage();
 
-//var xx = sm.SpiralMatrixSol( page);
+var xx = longestSubString.LongestSubStringSol(x);
 
-//Console.WriteLine(xx.ToString());
+Console.WriteLine(xx.ToString());
 
 
 //var res = combiSum.CombiSumSol(numz, target);
